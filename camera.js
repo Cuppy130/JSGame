829ac99a5e4216ce982a88cfecbdf427d9df4331
PixelChip <89292951+Cuppy130@users.noreply.github.com>
2023-10-17 05:31:47 +0000

    update

--- camera.js
+++ camera.js
@@ -1,11 +1,17 @@
 class camera2D {
-    constructor(){
+    constructor(ctx, renderCallback=()=>{}){
         this.pos = [0,0];
         this.following = [0,0];
+        this.ctx = ctx || new CanvasRenderingContext2D;
+        this.callback = renderCallback;
     }
     update(){
         for (let i = 0; i < this.pos.length; i++) {
-            this.pos[i] = (this.pos[i] + this.following[i]) / 2            
+            this.pos[i] = (this.pos[i] + this.following[i]) / 2;
         }
+    }
+    render(){
+        this.ctx.translate(pos[0], pos[1]);
+        this.callback();
     }
 }
