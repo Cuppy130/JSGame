b41d34ec26ef0e9320b3d7909e745e6e6d5726d9
PixelChip <89292951+Cuppy130@users.noreply.github.com>
2023-10-18 08:39:02 -0700

    push

--- classes/player.js
+++ classes/player.js
@@ -2,9 +2,19 @@ class playerClass{
     constructor(keys){
         this.keys = keys
         this.posision = []
+        this.keybinds = {
+            left: "KeyA",
+            right: "KeyD",
+            up: "KeyW",
+            down: "KeyS"
+        }
         //this.vel
     }
     update(){
+        let keys = this.keys
+        let kb = this.keybinds
+        if(keys.pressed(kb[0])){
 
+        }
     }
 }
