209290807f5c656b52ac7e658b520c561a4cff79
PixelChip <89292951+Cuppy130@users.noreply.github.com>
2023-10-18 20:55:36 +0000

    push

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { initializeApp } from 'firebase/app';
 
 function clamp (val, min, max) {return Math.min(Math.max(val, min), max)}
@@ -6,6 +7,45 @@ const keys = new keyHandler;
 const camera = new camera2D;
 const player = new Player(keys);
 const ctx = $("#gameWindow")[0].getContext('2d');
+=======
+import {  } from "firebase/app";
+import {  } from "firebase/auth";
+import {  } from "firebase/database";
+
+class Player{
+    constructor(keyboard){
+        this.keyboard = keyboard || null;
+        this.pos = {x: 0, y: 0};
+        this.drag = .6;
+        this.vel = {x: 0, y: 0};
+        this.gravity = 9.8;
+        this.keybinds = {
+            left: "KeyA",
+            right: "KeyD",
+            down: "KeyS",
+            up: "keyW"
+        };
+    }
+    update(){
+        
+    }
+}
+
+function createPlayer(keyboard, Callback){
+    const player = new Player(keyboard);
+    Callback(player)
+    return player;
+}
+
+function createObject(type){
+    switch (type){
+        default:
+            break;
+    }
+}
+
+const camera = new camera2D;
+>>>>>>> 45b11a0 (push)
 
 void function updateLoop(){
     camera.update();
