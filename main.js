8a371036bb1fa2b6830cfed1b96c2a61297d89af
PixelChip <89292951+Cuppy130@users.noreply.github.com>
2023-10-24 17:55:02 +0000

    Commit

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { initializeApp } from 'firebase/app';
 
 function clamp (val, min, max) {return Math.min(Math.max(val, min), max)}
@@ -7,45 +6,18 @@ const keys = new keyHandler;
 const camera = new camera2D;
 const player = new Player(keys);
 const ctx = $("#gameWindow")[0].getContext('2d');
-=======
+
 import {  } from "firebase/app";
 import {  } from "firebase/auth";
 import {  } from "firebase/database";
+import { Player } from "player"
 
-class Player{
-    constructor(keyboard){
-        this.keyboard = keyboard || null;
-        this.pos = {x: 0, y: 0};
-        this.drag = .6;
-        this.vel = {x: 0, y: 0};
-        this.gravity = 9.8;
-        this.keybinds = {
-            left: "KeyA",
-            right: "KeyD",
-            down: "KeyS",
-            up: "keyW"
-        };
-    }
-    update(){
-        
-    }
-}
+function clamp (val, min, max) {return Math.min(Math.max(val, min), max)}
 
-function createPlayer(keyboard, Callback){
-    const player = new Player(keyboard);
-    Callback(player)
-    return player;
-}
-
-function createObject(type){
-    switch (type){
-        default:
-            break;
-    }
-}
-
+const keys = new keyHandler;
 const camera = new camera2D;
->>>>>>> 45b11a0 (push)
+const player = new Player(keys);
+const ctx = $("#gameWindow")[0].getContext('2d');
 
 void function updateLoop(){
     camera.update();
