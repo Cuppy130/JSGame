1562a48f31d64a92a80fac6f6f9db44833e38184
PixelChip <89292951+Cuppy130@users.noreply.github.com>
2023-10-30 03:06:28 +0000

    Update

--- main.js
+++ main.js
@@ -9,65 +9,26 @@ function tileImage(imageSource, x, y, ax, ay, bs=30){
     ctx.fillRect(x*(bs*2), y*(bs*2)*-1, ax * bs*2, ay * bs*2);
     ctx.restore()
 }
-let zoom = 1.0;
+
 const ctx = $("#gameWindow")[0].getContext('2d')
-$("#gameWindow")[0].width = 320
-$("#gameWindow")[0].height = 240
 const keys = new keyHandler;
+
+const player = new PlayerClass
+//const playerCamera =
 
 const clamp = (val, min, max) => {
     return Math.max(Math.min(val, max), min);
 }
 
-class camera {
-    constructor(player){
-        this.pos = [0,0];
-        this.vel = [0,0];
-        this.follow = player.pos
+
+
+()=>{
+    const perframe = () => {
+        player.update(keys);
+        player.draw()
     }
-    update(){
-        this.vel[0] = this.pos[0] + this.follow[0] / 1.5
-    }
-    newFollow(array){
-        this.follow = array
-    }
-    draw(){
-        ctx.save();
-        ctx.translate(this.pos[0], this.pos[1])
-    }
+
+    setInterval(()=>{
+        perframe()
+    }, 1000/144)
 }
-class playerFake {
-}
-
-
-
-const player = new playerClass;
-const playerCamera = new camera(player)
-
-console.log("starting loops")
-
-//render loop
-setInterval(() => {
-    ctx.clearRect(0,0,640/2,480/2)
-    player.draw()
-    playerCamera.draw()
-}, 1000/60);
-
-function loop() {
-    player.update()
-    playerCamera.update()
-    requestAnimationFrame(loop);
-}
-
-blocks = {
-}
-
-for(let i=0; i<1; i++){
-    blocks[i] = new Image;
-    blocks[i].src = `blocks/${i}.webp`;
-}
-ctx.save()
-ctx.drawImage(blocks['0'], 20, 20, 20, 20)
-ctx.beginPath()
-ctx.rect(0, 0, 20, 20)
-ctx.restore()
--- player.js
+++ player.js
@@ -8,23 +8,36 @@ class PlayerClass {
     }
     update(keys){
         if(keys.pressed("KeyA") && keys.pressed("KeyD")) {
-            this.vel[0] /= 1.5;
+            this.vel[0] /= decelerate;
         } else if (keys.pressed("KeyA")){
             this.vel[0]-=this.speed;
         } else if (keys.pressed("KeyD")){
             this.vel[0]+=this.speed;
         } else {
-            this.vel[0] /= 1.5;
+            this.vel[0] /= decelerate;
         }
 
         if(keys.pressed("KeyW") && keys.pressed("KeyS")) {
-            this.vel[0] /= 1.5;
+            this.vel[0] /= decelerate;
         } else if (keys.pressed("KeyW")){
             this.vel[0]-=this.speed;
         } else if (keys.pressed("KeyS")){
             this.vel[0]+=this.speed;
         } else {
-            this.vel[0] /= 1.5;
+            this.vel[0] /= decelerate;
         }
+        //clamp
+        this.vel[0] = clamp(this.vel[0], -this.speedMax, this.speedMax)
+        this.vel[1] = clamp(this.vel[1], -this.speedMax, this.speedMax)
+
+        //update
+        this.pos[0] += this.vel[0];
+        this.pos[1] += this.vel[1];
+    }
+    draw(){
+        ctx.save()
+        ctx.translate(this.pos[0, pos[1]])
+        ctx.fillRect(-10, -10, 20, 20)
+        ctx.restore()
     }
 }
